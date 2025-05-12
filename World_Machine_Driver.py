b835a68f935291fb511048f19e62c31f33c1f319
Benjamin Gutman <benjamingutman@icloud.com>
2025-05-12 12:26:41 -0400

    added main function and available log function, added docstrings

--- World_Machine_Driver.py
+++ World_Machine_Driver.py
@@ -11,6 +11,7 @@ fileDict = {}
 
 
 def passwordDemand():
+    '''This function handles the password inputs and reacts accordingly'''
     userInput = (input("Enter Password: "))
     global passwordAttempts
     while userInput != passwordBase:
@@ -25,6 +26,7 @@ def passwordDemand():
 
 
 def readLogCollection():
+    '''This function reads the file World_Logs.txt and gathers the information from it to place into a dictionary'''
     file = open("World_Logs.txt", "r")
     lineCount = sum(1 for _ in file)
     file.seek(0)
@@ -38,6 +40,24 @@ def readLogCollection():
         fileDict[key] = value
     file.close()
 
+def availableLogs():
+    '''This function takes the information gathered from the file to determine what logs are available and print them
+    This is done by converting the keys of the dictionary into their own list'''
+    global fileDict
+    logs = list(fileDict.keys())
+    print("Logs Available:")
+    for i in range(len(logs)):
+        print(f"Log {logs[i]}")
 
-readLogCollection()
-print(fileDict["00"])
+def main(): #You don't need a main function but I'm so used to c++ that I decided to do it this way
+    if not passwordDemand():
+        return
+    readLogCollection()
+    availableLogs()
+    LogChoice = input("Which Log Would You Like To Read (Enter The Number Only): ")
+    Log = fileDict[LogChoice]
+    print("\nNow Printing Log: ")
+    print(Log)
+
+
+main()
